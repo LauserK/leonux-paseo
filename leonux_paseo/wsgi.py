0e2d9b5c9c2ddff33f130cb9f7433bb704de30b2
lauserk <kildarealauser@gmail.com>
2018-01-03 09:05:24 -0400

    Base project

---
+++ leonux_paseo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for leonux_paseo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "leonux_paseo.settings")
+
+application = get_wsgi_application()
