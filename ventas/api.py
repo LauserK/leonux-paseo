b85457fe8607a387308ef3060ffdbaf23773958b
lauserk <kildarealauser@gmail.com>
2018-06-06 17:16:30 -0400

    Added new services for IOS POS App VENTAS

--- ventas/api.py
+++ ventas/api.py
@@ -3,7 +3,8 @@ from django.views.generic import View
 from django.http import JsonResponse
 from django.db import connection, connections
 from articulos.views import dictfetchall
-import json
+import json, math
+from decimal import Decimal
 
 # Import Models
 from .models import Estacion, PuntoVentaDispositivo, Jornada
@@ -140,3 +141,174 @@ class Report(View):
             pass
 
         return APIResponse(None, "The report was saved", True)
+
+
+"""
+POS IOS SERVICES
+"""
+
+class Groups(View):
+    def post(self, request):
+        section = request.GET.get('section')
+        with connections['leonux'].cursor() as cursor:
+            cursor.execute("SELECT auto FROM pos_secciones WHERE codigo = %s", [section])
+            try:
+                auto = dictfetchall(cursor)[0]["auto"]
+                cursor.execute("SELECT pos_secciones_grupos.auto_grupo, productos_grupo.nombre FROM pos_secciones_grupos INNER JOIN productos_grupo ON productos_grupo.auto = pos_secciones_grupos.auto_grupo WHERE pos_secciones_grupos.auto_seccion = %s ORDER BY `productos_grupo`.`nombre` ASC", [auto])
+                groups = dictfetchall(cursor)
+            except:
+                return APIResponse(None, "the section doesnt exists", None)
+
+        return APIResponse(groups, "group list", True)
+
+class Articles(View):
+    def post(self, request):
+        group = request.GET.get('group')
+        article_code = request.GET.get('code')
+        article_plu = request.GET.get('plu')
+
+        with connections['leonux'].cursor() as cursor:
+            # if is only one article by his code
+            if article_code is not None:
+                cursor.execute("SELECT auto, codigo, nombre, tasa, precio_pto AS precio_neto FROM `productos`WHERE estatus = 'Activo' AND codigo = %s", [article_code])
+                
+                try:
+                    article = dictfetchall(cursor)[0]
+                except:
+                    return APIResponse(None, "The article not exists", False)    
+
+                return APIResponse(article, "article", True)
+
+            # if is only one article by his plu
+            if article_plu is not None:
+                cursor.execute("SELECT auto, codigo, nombre, tasa, precio_pto AS precio_neto FROM `productos`WHERE estatus = 'Activo' AND plu = %s", [article_plu])
+                
+                try:
+                    article = dictfetchall(cursor)[0]
+                except:
+                    return APIResponse(None, "The article not exists", False)    
+
+                return APIResponse(article, "article", True)
+
+            # if is all articles of a group
+            cursor.execute("SELECT auto, codigo, nombre, tasa, precio_pto AS precio_neto FROM `productos`WHERE estatus = 'Activo' AND auto_grupo = %s AND precio_pto != 0.00", [group])
+            articles = dictfetchall(cursor)
+
+        return APIResponse(articles, "article list", True)
+
+class GetAllArticlesAccount(View):
+    def post(self, request):
+        with connections['leonux'].cursor() as cursor:
+            json_data = json.loads(request.body)
+            client  = json_data["client"]
+            # Get the last 5 digits of the client code and add zeros until the string len be 5
+            account = str(client["code"][-5:]).zfill(5)
+            print account
+
+            cursor.execute("SELECT auto, auto_producto, nombre, codigo, cantidad, precio_item as precio_neto, tasa FROM pos_comandas WHERE cuenta = %s", [account])
+            articles = dictfetchall(cursor)
+
+        return APIResponse(articles, "article list", True)  
+
+class AddArticleAccount(View):
+    def post(self, request):
+        json_data = json.loads(request.body)
+
+        article_data   = json_data["article"]
+        client    = json_data["client"]
+        # Get the last 5 digits of the client code and add zeros until the string len be 5
+        account    = str(client["code"][-5:]).zfill(5)
+        auto_nuevo = ""
+
+        with connections['leonux'].cursor() as cursor:
+            cursor.execute("SELECT auto FROM pos_cuentas WHERE cuenta = %s", [account])
+            accounts = dictfetchall(cursor)
+
+            try:
+                auto_nuevo = accounts[0]["auto"]
+            except:pass
+
+            if not len(accounts) > 0:
+                """
+                If the account doesnt exists create the new account with the last 4 digits of client code
+                """
+                cursor.execute("SELECT a_pos_cuentas FROM sistema_contadores limit 1")
+                auto = int(dictfetchall(cursor)[0]["a_pos_cuentas"])
+                auto_nuevo = str(auto + 1).zfill(10)
+
+                cursor.execute("INSERT INTO `00000001`.`pos_cuentas` (`auto`, `cuenta`, `estatus_cuenta`, `estatus_servicio`, `estatus_abierta`, `estatus`, `acumulado`, `auto_cliente`, `ci_rif`, `nombre`, `dir_fiscal`, `hora`, `fin`, `corte`) VALUES (%s, %s, '0', '0', '0', 'Activo', '0.00', '', '', '', '', '', '', '0')", [auto_nuevo, account])
+
+                cursor.execute("UPDATE `00000001`.`sistema_contadores` SET `a_pos_cuentas` = %s WHERE a_pos_cuentas != '' LIMIT 1", [auto_nuevo])    
+
+            """
+            Add the article to the account
+            """
+            cursor.execute("SELECT auto, nombre, codigo, auto_departamento, auto_grupo, auto_subgrupo, auto_tasa, precio_pto, tasa FROM productos WHERE codigo = %s", [article_data["code"]])
+            
+            try:
+                article = dictfetchall(cursor)[0]
+            except:
+                return APIResponse(None, "article not exists", False)
+            
+            # Totals
+            quantity = "%.3f" % float(article_data["quantity"])
+            amount_global = Decimal("%.2f" % round(article["precio_pto"] * Decimal(quantity),2))
+            tax = Decimal("%.2f" % round(Decimal(round((article["precio_pto"] * article["tasa"] / 100),2)) * Decimal(quantity),2))
+            total = amount_global + tax
+
+
+            cursor.execute("SELECT a_pos_comandas FROM sistema_contadores limit 1")
+            auto = int(dictfetchall(cursor)[0]["a_pos_comandas"])
+            auto_cuenta_row = str(auto + 1).zfill(10)
+
+            cursor.execute("INSERT INTO pos_comandas (`auto`, `auto_producto`, `codigo`, `nombre`, `auto_departamento`, `auto_grupo`, `auto_subgrupo`, `auto_deposito`, `cantidad`, `empaque`, `precio_neto`, `descuento1p`, `descuento1`, `costo_venta`, `total_neto`, `tasa`, `impuesto`, `total`, `fecha`, `hora`, `deposito`, `precio_final`, `decimales`, `contenido_empaque`, `cantidad_und`, `precio_und`, `costo_und`, `precio_item`, `codigo_deposito`, `detalle`, `auto_tasa`, `categoria`, `costo_promedio_und`, `costo_compra`, `estatus_comanda`, `total_descuento`, `auto_vendedor`, `codigo_vendedor`, `auto_cuenta`, `cuenta`, `nombre_vendedor`, `detalle_cambio`) VALUES (%s, %s, %s, %s, %s, %s, %s, '0000000001', %s, 'UNIDAD', %s, '0.00', '0.00', '0.00', %s, %s, %s, %s, '2018-05-30', '20:00', 'PRINCIPAL',%s, '3', '0', '0.000', %s, '0.00', %s, '01', '', %s, '', '0.00', '0.00', '0', '0.00', '0000000001', '01', %s, %s, 'DIRECTO', '');", [auto_cuenta_row, article["auto"], article["codigo"], article["nombre"], article["auto_departamento"], article["auto_grupo"], article["auto_subgrupo"], quantity, article["precio_pto"], amount_global, article["tasa"], tax, total, article["precio_pto"], article["precio_pto"], article["precio_pto"], article["auto_tasa"], auto_nuevo, account])
+
+            cursor.execute("UPDATE `00000001`.`sistema_contadores` SET `a_pos_comandas` = %s WHERE a_pos_comandas != '' LIMIT 1", [auto_cuenta_row])
+            
+            ctx = [{
+                "auto": auto_cuenta_row
+            }]
+        return APIResponse(ctx, "saved", True)
+
+
+class RemoveArticleAccount(View):
+    def post(self, request):
+        json_data    = json.loads(request.body)
+        article_data = json_data["article"]
+
+        with connections['leonux'].cursor() as cursor:
+            cursor.execute("DELETE FROM pos_comandas WHERE auto = %s", [article_data["auto_row"]])
+
+        return APIResponse(None, "removed", True)
+
+class RemoveAllArticleAccount(View):
+    def post(self, request):
+        json_data    = json.loads(request.body)
+        client    = json_data["client"]
+        # Get the last 4 digits of the client code and add zeros until the string len be 5
+        account    = str(client["code"][-5:]).zfill(5)
+
+        with connections['leonux'].cursor() as cursor:
+            cursor.execute("DELETE FROM pos_comandas WHERE cuenta = %s", [account])
+
+        return APIResponse(None, "removed", True)
+
+
+class GetClientByQueue(View):
+    def post(self, request):
+        section = request.GET.get('section')
+        status  = request.GET.get('status') or '0'
+
+        with connections['leonux'].cursor() as cursor:
+            cursor.execute("SELECT id, pos_turno.rif, pos_turno.razon_social, pos_turno.ci as codigo, pos_turno.nombre, clientes.auto FROM `pos_turno` INNER JOIN clientes ON clientes.codigo = pos_turno.ci WHERE seccion = %s AND pos_turno.estatus = %s", [section, status])
+            clients = dictfetchall(cursor)
+
+        return APIResponse(clients, "client list", True)
+
+class UpdateClientStatus(View):
+    def post(self, request):
+        client = request.GET.get('auto')
+        status = request.GET.get('status')
+        with connections['leonux'].cursor() as cursor:
+            cursor.execute("UPDATE pos_turno SET estatus = %s WHERE id = %s", [status, client])
+            return APIResponse(None, "updated", True)
--- ventas/api_urls.py
+++ ventas/api_urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .api import Stations, Devices, Sessions, Tickets, Report
+from .api import Stations, Devices, Sessions, Tickets, Report, Groups, Articles, AddArticleAccount, RemoveArticleAccount, RemoveAllArticleAccount, GetAllArticlesAccount, GetClientByQueue, UpdateClientStatus
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
@@ -9,4 +9,14 @@ urlpatterns = [
     url(r'^stations/(?P<station_id>\d+)/devices/$', Devices.as_view(), name='devices-api'),
     url(r'^stations/(?P<station_id>\d+)/session/$', Sessions.as_view(), name='sessions-api'),
     url(r'^stations/(?P<station_id>\d+)/session/tickets/(?P<ticket_number>\d+)/$', Tickets.as_view(), name='sessions-tickets-api'),
+
+    # POS IOS
+    url(r'^clients/$', csrf_exempt(GetClientByQueue.as_view()), name='clients-api'),
+    url(r'^clients/update/$', csrf_exempt(UpdateClientStatus.as_view()), name='clients-api'),
+    url(r'^groups/$', csrf_exempt(Groups.as_view()), name='groups-api'),
+    url(r'^articles/$', csrf_exempt(Articles.as_view()), name='articles-api'),
+    url(r'^articles/account/$', csrf_exempt(GetAllArticlesAccount.as_view()), name='articles-api'),
+    url(r'^articles/add/$', csrf_exempt(AddArticleAccount.as_view()), name='articles-api'),
+    url(r'^articles/remove/$', csrf_exempt(RemoveArticleAccount.as_view()), name='articles-api'),
+    url(r'^articles/remove/all/$', csrf_exempt(RemoveAllArticleAccount.as_view()), name='articles-api'),
 ]
