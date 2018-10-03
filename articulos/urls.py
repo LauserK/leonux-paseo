391735fb4a2acbf7534b39c15ff2b743a89d660f
lauserk <kildarealauser@gmail.com>
2018-10-03 12:31:11 -0400

    Added VentasUsuario reporte

--- articulos/urls.py
+++ articulos/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import ArticuloReporteView, ReporteMenuView, VentasReporteView
+from .views import ArticuloReporteView, ReporteMenuView, VentasReporteView, VentaUsuarioReporteView
 
 urlpatterns = [
     # Reporte articulos
     url(r'^$', ReporteMenuView.as_view(), name='reporte-home'),
     url(r'^articulos/$', ArticuloReporteView.as_view(), name='reporte-articulos'),
     url(r'^ventas/$', VentasReporteView.as_view(), name='reporte-ventas'),
+    url(r'^ventas-usuario/$', VentaUsuarioReporteView.as_view(), name='reporte-ventas'),
 ]
--- articulos/views.py
+++ articulos/views.py
@@ -162,7 +162,8 @@ class ArticuloReporteView(LoginRequiredMixin, View):
                     "codigo": articulo["codigo"],
                     "nombre": articulo["nombre"],
                     "depositos": [],
-                    "cantidad_vendida": 0
+                    "cantidad_vendida": 0,
+                    "cantidad_depositos": 0.00
                 }
 
                 # Obtenemos los depositos
@@ -171,6 +172,7 @@ class ArticuloReporteView(LoginRequiredMixin, View):
                 depositos = dictfetchall(cursor)
                 for deposito in depositos:
                     articulo_objeto["depositos"].append(deposito)
+                    articulo_objeto["cantidad_depositos"] = float(articulo_objeto["cantidad_depositos"]) + float(deposito["fisica"])                
 
                 cursor.execute("SELECT SUM(cantidad) as cantidad FROM ventas_detalle WHERE auto_producto = %s AND fecha >= %s AND fecha <= %s GROUP BY auto_producto", [articulo["auto"], desde, hasta])
                 try:
@@ -289,4 +291,171 @@ class VentasReporteView(View):
         }
         #template = "reportes/ventas/ventas-reporte-grafica.html"
         template = "reportes/ventas/ventas-reporte-lista.html"
+        return render(request, template, ctx)
+
+class VentaUsuarioReporteView(View):
+    def get(self, request):
+        if not request.user.tipo_usuario.reportes.filter(url="articulos").exists():
+            raise Http404("NO TIENES PERMISOS PARA VISUALIZAR ESTA PÁGINA")
+
+        with connections['leonux'].cursor() as cursor:
+            # Obtener los proveedores
+            cursor.execute("SELECT auto, razon_social, ci_rif FROM proveedores WHERE estatus = 'Activo' ORDER BY razon_social")
+            proveedores = dictfetchall(cursor)
+
+            # Obtener los departamentos del sistema
+            cursor.execute("SELECT auto, nombre FROM empresa_departamentos ORDER BY nombre")
+            departamentos = dictfetchall(cursor)
+
+            # Obtener los grupos/familias de los articulos
+            cursor.execute("SELECT auto, nombre FROM productos_grupo ORDER BY nombre")
+            grupos = dictfetchall(cursor)
+
+            # Obtener las marcas de los articulos
+            cursor.execute("SELECT auto, nombre FROM productos_marca ORDER BY nombre")
+            marcas = dictfetchall(cursor)
+
+            cursor.close()
+
+            # Lista de categorias en el sistema
+            categorias = [{"nombre": "Bien de Servicio"},{"nombre": "Materia Prima"},{"nombre": "Producto Terminado"},{"nombre": "Uso Interno"},{"nombre": "Producto Manufacturado"}]
+
+        ctx = {
+            "proveedores": proveedores,
+            "departamentos": departamentos,
+            "grupos": grupos,
+            "subgrupos": "",
+            "marcas": marcas,
+            "categorias": categorias
+        }
+        template = "reportes/ventas/ventasUsuario-index.html"
+        return render(request, template, ctx)
+
+    def post(self, request):
+        # Obtenemos los filtros del formulario
+        auto_proveedor    = request.POST.get("filtro-proveedor")
+        referencia        = request.POST.get("filtro-referencia")
+        auto_departamento = request.POST.get("filtro-departamento")
+        auto_grupo        = request.POST.get("filtro-grupos")
+        auto_marca        = request.POST.get("filtro-marcas")
+        categoria         = request.POST.get("filtro-categorias")
+        tipo_reporte      = request.POST.get("tipo-reporte")
+        tipo_orden        = request.POST.get("orden") # WIP
+
+        with connections['leonux'].cursor() as cursor:
+            # Filtros
+            filtros = ""
+
+            # Query sin filtros
+            sql = "SELECT ventas_detalle.auto_producto AS auto, productos.codigo, productos.nombre FROM ventas_detalle INNER JOIN productos ON ventas_detalle.auto_producto = productos.auto WHERE productos.estatus = 'Activo'"
+
+            # Obtener los articulos del proveedor mediante el filtro
+            proveedor = {}
+            if auto_proveedor is not None and auto_proveedor != "":
+                cursor.execute("SELECT auto, razon_social, ci_rif FROM proveedores WHERE auto = %s", [auto_proveedor])
+                proveedor = dictfetchall(cursor)[0]
+                sql = "SELECT compras_detalle.auto_producto AS auto, compras_detalle.codigo, compras_detalle.nombre, productos.estatus FROM compras_detalle INNER JOIN compras ON compras.auto = compras_detalle.auto_documento INNER JOIN productos ON compras_detalle.auto_producto = productos.auto WHERE compras.auto_proveedor = %s and productos.estatus = 'Activo'" % auto_proveedor
+
+            # Filtro por referencia (SI ES MARCADO O NO / 0|1)
+            if referencia is not None and referencia != "":
+                filtros = "%s AND productos.referencia = %s" % (filtros, referencia)
+
+            # Obtener info del filtro (departamento)
+            departamento = ""
+            if auto_departamento is not None and auto_departamento != "":
+                cursor.execute("SELECT auto, nombre FROM empresa_departamentos WHERE auto = %s", [auto_departamento])
+                departamento = dictfetchall(cursor)[0]
+
+                filtros = "%s AND productos.auto_departamento = %s" % (filtros, auto_departamento)
+
+            # Obtener info del filtro (grupo)
+            grupo = ""
+            if auto_grupo is not None and auto_grupo != "":
+                # Obtener e grupos/familias del filtro
+                cursor.execute("SELECT auto, nombre FROM productos_grupo WHERE auto = %s", [auto_grupo])
+                grupo = dictfetchall(cursor)[0]
+
+                filtros = "%s AND productos.auto_grupo = %s" % (filtros, auto_grupo)
+
+            marca = ""
+            if auto_marca is not None and auto_marca != "":
+            # Obtener la marcas del filtro
+                cursor.execute("SELECT auto, nombre FROM productos_marca WHERE auto = %s", [auto_marca])
+                marca = dictfetchall(cursor)[0]
+
+                filtros = "%s AND productos.auto_marca = %s" % (filtros, auto_marca)
+
+            if categoria is not None and categoria != "":
+                filtros = "%s AND productos.categoria = '%s'" % (filtros, categoria)
+
+
+            # Obtener los articulos
+            sql_completo = sql + filtros + " GROUP BY auto"
+            cursor.execute(sql_completo)
+            articulos = dictfetchall(cursor)
+
+            # Filtro de fechas para ventas
+            filtro_ventas = request.POST.get("filtro-dias") # dias || fechas
+            dias = 0
+            desde = ""
+            hasta = str(datetime.today().date()) # Por default hasta el dia de hoy
+
+            # si es por dias (Hoy, dia anterior, ultimos 15 dias, etc)
+            if filtro_ventas == "dias":
+                dias = request.POST.get("filtro-dia")
+                d = datetime.today() - timedelta(days=int(dias)) # Desde hoy descuenta los dias indicados
+                desde = str(d.date()) # Lo pasamos a string
+                if dias == "1": # Si la opcion es "Dia Anterior" solo mostramos los datos de ese dia especfico
+                    hasta = desde
+            elif filtro_ventas == "fechas": # Si es por rango de fechas
+                desde = request.POST.get("filtro-desde")
+                if request.POST.get("filtro-hasta") != "" and request.POST.get("filtro-hasta") is not None: # SI no se pasa una fecha es hasta hoy
+                    hasta = request.POST.get("filtro-hasta")
+
+
+            # Damos formato a los datos
+            articulos_array = []
+            for articulo in articulos:
+                articulo_objeto = {
+                    "codigo": articulo["codigo"],
+                    "nombre": articulo["nombre"],
+                    "usuarios": [],
+                    "cantidad_vendida": 0,
+                    "cantidad_depositos": 0.00
+                }
+             
+                cursor.execute("SELECT ventas.usuario as nombre, SUM( ventas_detalle.cantidad ) AS cantidad FROM ventas_detalle INNER JOIN ventas ON ventas_detalle.auto_documento = ventas.auto WHERE ventas_detalle.auto_producto = %s AND ventas_detalle.fecha >= %s AND ventas_detalle.fecha <= %s GROUP BY ventas_detalle.nombre, ventas.usuario", [articulo["auto"], desde, hasta])
+                # Ingresamos los datos al objeto del articulo
+                usuarios = dictfetchall(cursor)
+
+                for usuario in usuarios:                    
+                    articulo_objeto["usuarios"].append(usuario)              
+
+                cursor.execute("SELECT SUM(cantidad) as cantidad FROM ventas_detalle WHERE auto_producto = %s AND fecha >= %s AND fecha <= %s GROUP BY auto_producto", [articulo["auto"], desde, hasta])
+                try:
+                    cantidad = dictfetchall(cursor)[0]
+                    articulo_objeto["cantidad_vendida"] = int(cantidad["cantidad"])
+                except:
+                    pass
+
+                # Agregamos el articulo a la lista
+                articulos_array.append(articulo_objeto)
+
+            # Cerramos la conexion
+            cursor.close()
+
+
+        ctx        = {
+            "articulos": articulos_array,
+            "proveedor": proveedor,
+            "referencia": referencia,
+            "departamento": departamento,
+            "grupo": grupo,
+            "marca": marca,
+            "categoria": categoria,
+            "fecha": str(datetime.today().date().strftime('%d-%m-%Y')),
+            "desde":desde,
+            "hasta":hasta
+        }
+        template   = "reportes/ventas/ventasUsuario-reporte-lista.html"
         return render(request, template, ctx)
